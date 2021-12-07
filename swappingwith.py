80387da3edb43de377741bb6cd68a25aefa9da8d
vaishnaviu08 <95694509+vaishnaviu08@users.noreply.github.com>
2021-12-07 18:46:01 +0530

    Add files via upload

---
+++ swappingwith.py
@@ -0,0 +1,12 @@
+a=int(input("Enter a first no."))
+b=int(input("Enter the second no."))
+c=int
+print("Before Swapping")
+print("a : {}".format(a))
+print("b : {}".format(b))
+temp=a
+a=b
+b=temp
+print("After Swapping")
+print("a : {}".format(a))
+print("b : {}".format(b))
